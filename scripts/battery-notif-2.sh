b1a2224c67c5e29a9c5718c58d1a6d203e51ca09
jarman <jarukitman@yahoo.com>
2025-01-11 12:20:59 +0700

    deleted the symbolic link:    scripts 	new file:   scripts/battery-notif-2.sh 	new file:   scripts/battery-notif.sh 	new file:   scripts/random-wallpaper.sh 	new file:   scripts/screenshot.sh

---
+++ scripts/battery-notif-2.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+#lib-notify acpi
+
+
+notify-send "Battery Info" "$(acpi -b)\nTime: $(timedatectl | grep -Eo -m 1 "[0-9]+:[0-9]+:[0-9]+")" --app-name="BATTERY"
+swaync-client --close-latest
