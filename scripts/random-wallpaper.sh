447e1fb3090dc38e475bf05d56d707930f86a185
JarukitMan <pleaseletmeinijustwanttoemail@gmail.com>
2025-07-13 21:06:58 +0700

    Some things changed. I also only just now noticed the nested scripts folder. 	new file:   electron-flags.conf 	modified:   fastfetch/config.jsonc 	modified:   hypr/hyprland.conf 	modified:   kitty/kitty.conf 	modified:   kitty/kitty.conf.bak 	modified:   scripts/random-wallpaper.sh 	deleted:    scripts/scripts/battery-notif-2.sh 	deleted:    scripts/scripts/battery-notif.sh 	deleted:    scripts/scripts/random-wallpaper.sh 	deleted:    scripts/scripts/screenshot.sh 	deleted:    scripts/scripts/y.sh 	modified:   swaync/style.css 	new file:   vesktop-flags.conf

--- scripts/random-wallpaper.sh
+++ scripts/random-wallpaper.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-sleep .1
-
+sleep 0.1
 while true
 do
-	hyprctl hyprpaper unload all
 	wallpapers=($(ls -d ~/Images/Wallpapers/*))
 	wallpaper=${wallpapers[ $RANDOM % ${#wallpapers[@]}]}
 
-	sleep .1
-	hyprctl hyprpaper preload $wallpaper
-	sleep .1
-	hyprctl hyprpaper wallpaper ,$wallpaper
+	# sleep 0.1
+	# hyprctl hyprpaper unload all
+	# # sleep 0.1
+	# hyprctl hyprpaper preload $wallpaper
+	# # sleep 0.1
+	# hyprctl hyprpaper wallpaper ,$wallpaper
+	hyprctl hyprpaper reload ,$wallpaper
 
 sleep 1800
 done
--- scripts/scripts/screenshot.sh
+++
@@ -1,2 +0,0 @@
-grim -g "$(slurp)" $(xdg-user-dir PICTURES)/Screenshots/$(date +'%s_grim.png')
-wl-copy < $(xdg-user-dir PICTURES)/Screenshots/$(date +'%s_grim.png')
